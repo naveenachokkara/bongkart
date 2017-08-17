dc924d0ad6042d45657bd642cc55768a6e406cc4
gdpwork4491 <gdpwork4491@gmail.com>
2017-08-17 16:56:56 +0530

    Updated logic to find server uri

--- CMS/app/constants/server.js
+++ CMS/app/constants/server.js
@@ -3,7 +3,13 @@
  */
 (function() {
     'use strict';
+    var serverURI = "";
+    if (!window.location.hostname || window.location.hostname.indexOf("localhost") >= 0) {
+        serverURI = "http://50.63.165.179:3000/";
+    } else {
+        serverURI = location.origin + ":3000/";
+    }
     angular
         .module('bongKart.bong')
-        .constant('SERVER_API', 'http://localhost:3000/');
+        .constant('SERVER_API', serverURI);
 })();
