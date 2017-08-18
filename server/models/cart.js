ac1c6a1fa72170da464a1985c0f808cdda31c61e
gdpwork4491 <gdpwork4491@gmail.com>
2017-08-18 17:26:02 +0530

    Added mongoose-id-validator and Implemented apis - add item to cart , update item in cart and get cart details.

--- server/models/cart.js
+++ server/models/cart.js
@@ -1,13 +1,19 @@
 'use strict';
 const mongoose = require('mongoose');
+const idvalidator = require('mongoose-id-validator');
 const Schema = mongoose.Schema;
 var CartSchema = mongoose.Schema({
-    "deviceId":String,
-    "user":{type: Schema.Types.ObjectId, ref: 'User' },
-    "item":{type: Schema.Types.ObjectId, ref: 'Bong' },
+    "deviceId":{type:String,require:true},
+    "userId":{type: Schema.Types.ObjectId, ref: 'User' },
+    "items":[{"bongId":{type: Schema.Types.ObjectId, ref: 'Bong', require:true},"quantity":{type:Number,require:true}}],
     "created": {
         type: Date,
         default: Date.now
-    }
+    },
+    "updated": {
+        type: Date,
+        default: Date.now
+    }    
 });
+CartSchema.plugin(idvalidator);
 const cart = module.exports = mongoose.model("cart",CartSchema);
--- server/routes/cart.js
+++ server/routes/cart.js
@@ -11,21 +11,63 @@ const extend = require('extend');
 const _ = require('lodash');
 var ObjectId = mongoose.Types.ObjectId;
 
-router.post('/addItem',(req,res,next) => {
+router.post('/addItem', (req, res) => {
     var reqData = {};
     reqData.deviceId = req.body.deviceId;
-    reqData.user = new ObjectId(req.body.userId);
-    reqData.item = new ObjectId(req.body.item);
-    let newCart = new Cart(reqData);
-    newCart.save((err,cart) => {
-        if(err){
-            res.json({status:'failure'});
-        }
-        else{
-            res.json({status:'Saved successfully'});
-    }
-});
+    reqData.userId = req.body.userId ? req.body.userId : null;
+    reqData.item = req.body.item;
+    Cart.findOneAndUpdate({
+        "deviceId": req.body.deviceId
+    }, {
+            "$push": { "items": reqData.item },
+            'updated': new Date().toISOString(),
+            "userId": reqData.userId
+        }, {
+            upsert: true,
+            setDefaultsOnInsert: true,
+            new: true,
+            runValidators: true
+        }, function (err, cart) {
+            if (err) {
+                console.log(err);
+                res.json({ "status": "error" });
+            } else {
+                res.json(cart);
+            }
+        });
 });
 
-
+router.post('/updateItem', (req, res) => {
+    var reqData = {};
+    reqData.deviceId = req.body.deviceId;
+    reqData.item = req.body.item;
+    console.log(reqData);
+    Cart.update({
+        "deviceId": req.body.deviceId,
+        "items": { "$elemMatch": { "bongId": new ObjectId(reqData.item.bongId) } }
+    }, 
+     { $set: { "items.$.quantity": reqData.item.quantity } }, 
+        function (err, cart) {
+            if (err) {
+                console.log(err);
+                res.json({ "status": "error" });
+            } else {
+                res.json(cart);
+            }
+        });
+});
+router.get('/details', (req, res) => {
+    console.log(req.query.deviceId);
+    Cart.findOne({
+        "deviceId": req.query.deviceId
+    },  
+        function (err, cart) {
+            if (err) {
+                console.log(err);
+                res.json({ "status": "error" });
+            } else {
+                res.json(cart);
+            }
+        });
+});
 module.exports = router;
