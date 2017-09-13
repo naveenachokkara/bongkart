f06701e45c7dcc257c5cb9e56c28206c9c127dbc
gdpwork4491 <gdpwork4491@gmail.com>
2017-09-13 21:44:48 +0530

    Update user api response

--- server/routes/user.js
+++ server/routes/user.js
@@ -6,63 +6,66 @@ const express = require('express');
 const router = express.Router();
 const user = require('../models/user');
 
-router.post('/create',(req,res,next) => {
+router.post('/create', (req, res, next) => {
     console.log(req.body);
     let newUser = new user(req.body);
-    newUser.save((err,user) => {
-    if(err){
-        res.json(err);
-    }
-    else{
-        res.json(user);
-}
-});
-});
-
-
-router.get('/list',(req,res,next) => {
-    user.find({},(err,users) => {
-    if(err){
-        res.json({status:'failure'});
-    }
-    else{
-        res.json(users);
-}
-});
-});
-
-router.get('/:id',(req,res,next) => {
-    user.find({_id:req.params.id},(err,user) => {
-    if(err){
-        res.json({status:'failure'});
-    }
-    else{
-        res.json(user);
-}
-});
+    newUser.save((err, user) => {
+        if (err) {
+            res.json(err);
+        }
+        else {
+            res.json(user);
+        }
+    });
 });
 
 
-router.delete('/:id',(req,res,next) => {
-    user.remove({_id:req.params.id},(err,user) => {
-    if(err){
-        res.json({status:'failure'});
-    }
-    else{
-        res.json(user);
-}
-});
+router.get('/list', (req, res, next) => {
+    user.find({}, (err, users) => {
+        if (err) {
+            res.json({ status: 'failure' });
+        }
+        else {
+            res.json(users);
+        }
+    });
 });
 
-router.put('/update/:id',(req,res,next) => {
-    user.findOneAndUpdate({_id:req.params.id},req.body,(err,user) => {
-    if(err){
-        res.json({status:'failure'});
-    }
-    else{
-        res.json({status:'User is updated successfully'});
-}
+router.get('/:id', (req, res, next) => {
+    user.find({ _id: req.params.id }, (err, user) => {
+        if (err) {
+            res.json({ status: 'failure' });
+        }
+        else {
+            res.json(user);
+        }
+    });
 });
+
+
+router.delete('/:id', (req, res, next) => {
+    user.remove({ _id: req.params.id }, (err, user) => {
+        if (err) {
+            res.json({ status: 'failure' });
+        }
+        else {
+            res.json(user);
+        }
+    });
+});
+
+router.put('/update/:id', (req, res, next) => {
+    user.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true
+    }, (err, user) => {
+        if (err) {
+            res.json({ status: 'failure' });
+        }
+        else {
+            res.json(user);
+        }
+    });
 });
 
 module.exports = router;
