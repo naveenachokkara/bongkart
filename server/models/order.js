048d5c8ce6c230759c474650b6d5f8bb6a6b3839
gdpwork4491 <gdpwork4491@gmail.com>
2017-09-26 11:59:15 +0530

    Updated Order model and implemented place order api.

--- server/models/order.js
+++ server/models/order.js
@@ -2,17 +2,29 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 var OrderSchema = mongoose.Schema({
-    "order":[{
-        "items":[
-            {
-                "id":String,
-                "expectedDeliveryDate":Date,
-                "deliveryDate":Date,
-                "status":String,
-                "item":{type: Schema.Types.ObjectId, ref: 'Bong' }
-            }
-        ],
-        "user":{type: Schema.Types.ObjectId, ref: 'User' }
-    }]
+    "created": {
+        type: Date,
+        default: Date.now
+    },
+    "updated": {
+        type: Date,
+        default: Date.now
+    },    
+    "userId":{type: Schema.Types.ObjectId, ref: 'User' },
+    "addressId":{type: Schema.Types.ObjectId, ref: 'address' },
+    "paymentMethod":String,
+    "status": String,
+    "expectedDeliveryDate": { type: Date }, 
+    "deliveryDate": {type: Date},
+    "tax": {type:Number,default:0},
+    "totalAmount": Number, 
+    "items": [
+        {
+            "bongId":{type: Schema.Types.ObjectId, ref: 'Bong', require:true},
+            "quantity": {type:Number,require:true},
+            "price": Number,
+            "originalPrice": Number
+        }
+    ]
 });
 const order = module.exports = mongoose.model("order",OrderSchema);
--- server/routes/order.js
+++ server/routes/order.js
@@ -3,19 +3,76 @@
  */
 'use strict';
 const express = require("express");
+const mongoose = require('mongoose');
 const router = express.Router();
 const order = require('../models/order');
-
-router.post('/create',(req,res,next) => {
-    let newOrder = new order(req.body);
-    newOrder.save((err,order) => {
-        if(err){
-            res.json(err);
-        }
-        else{
-            res.json(order);
-        }
-    });
+const Cart = require('../models/cart');
+const _ = require('underscore');
+var ObjectId = mongoose.Types.ObjectId;
+router.post('/placeOrder',(req,res,next) => {
+    if(req.body.userId && req.body.addressId && req.body.expectedDeliveryDate){
+        getCartDetails(req.body,function(err,carts){
+            if(err){
+                res.json({ "status": "error", "error": err });
+            } else if(carts && carts.length){
+                var items = [];
+                var totalAmount = 0;
+                var products = [];
+                var orderObj = {
+                    "userId": req.body.userId,
+                    "addressId": req.body.addressId,
+                    "paymentMethod": null,
+                    "status": "pending",
+                    "expectedDeliveryDate": new Date(req.body.expectedDeliveryDate),
+                    "deliveryDate": null,
+                    "tax": 0
+                }
+                _.each(carts[0].items, function (item) {
+                    var product = _.find(carts[0].products, function (product) {
+                        return product._id.equals(item.bongId);
+                    });
+                    if (product) {
+                        totalAmount += product.price;
+                        items.push({
+                            "bongId": item.bongId,
+                            "quantity": item.quantity,
+                            "price": product.price,
+                            "originalPrice": product.originalPrice
+                        });
+                        products.push(product);
+                  }
+                });
+                orderObj.totalAmount = totalAmount;
+                orderObj.items = items;
+                let newOrder = new order(orderObj);
+                newOrder.save((err, order) => {
+                    if (err) {
+                        res.json({ "status": "error", message: "Order creation error", error: err });
+                    } else {
+                        order = JSON.parse(JSON.stringify(order))
+                        order.products = products;
+                        Cart.findOneAndRemove(
+                            {
+                                "_id": carts[0]._id
+                            },
+                            function (err, cart) {
+                                console.log(cart);
+                                if (err) {
+                                    console.log("User Cart with id - " + carts[0]._id + " deleted failed - userId - "+ req.body.userId);
+                                } else {
+                                    console.log("User Cart with id - " + carts[0]._id + " deleted Succcessfully - userId"+ req.body.userId);
+                                }
+                            });
+                        res.json(order);
+                    }
+                });
+            } else {
+                res.json({ "status": "error", "message": "Cart not found" });
+            }
+        });
+    } else {
+        res.json({ "status": "error", "message": "invalid inputs" });
+    }
 });
 
 
@@ -65,5 +122,40 @@ router.delete("/delete/:id",(req,res,next) =>{
         }
     });
 });
-
+function getCartDetails(reqData, callback) {
+    var matchQuery = { "$match": {} };
+    if (reqData.userId) {
+        matchQuery["$match"]["userId"] = new ObjectId(reqData.userId);
+    } else if (reqData.deviceId) {
+        matchQuery["$match"]["deviceId"] = reqData.deviceId;
+    }
+    Cart.aggregate([
+        matchQuery,
+        {
+            "$unwind": { "path": "$items", "preserveNullAndEmptyArrays": true }
+        }, {
+            "$lookup": { "from": "bongs", "localField": "items.bongId", "foreignField": "_id", as: "products" }
+        }, {
+            "$unwind": "$products"
+        }, {
+            "$group": {
+                "_id": "$_id",
+                "items": { "$push": "$items" },
+                "products": { "$push": "$products" }
+            }
+        }], function (err, cart) {
+            if (err) {
+                callback(err);
+            } else {
+                if (cart && cart[0]) {
+                    _.each(cart[0].products, function (bong) {
+                        _.each(bong.images, function (image) {
+                            image.imageUrl = image.url + "s/" + image.file.name;
+                        })
+                    });
+                }
+                callback(null, cart);
+            }
+        });
+}
 module.exports = router;
