7dc7b485df17dd03c4a3fe8e14736f11832e18b4
gdpwork4491 <gdpwork4491@gmail.com>
2017-10-28 20:00:57 +0530

    Created dev changes

--- admin/src/app/AppConfig.ts
+++ admin/src/app/AppConfig.ts
@@ -1,4 +1,4 @@
 export const AppConfig = {
-    "baseURL": "http://50.63.165.179:3000/"
-    //"baseURL": "http://127.0.0.1:3000/"
+    //"baseURL": "http://50.63.165.179:3000/"
+    "baseURL": "http://127.0.0.1:3000/"
 };
